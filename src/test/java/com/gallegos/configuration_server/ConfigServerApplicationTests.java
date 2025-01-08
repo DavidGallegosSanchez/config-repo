2c2ea1b6ec6aa6e5a7e85833b1bb22cb5547c528
David Gallegos <dgallegos1985@gmail.com>
2025-01-08 16:39:45 +0100

    config structure

---
+++ src/test/java/com/gallegos/configuration_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gallegos.configuration_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
